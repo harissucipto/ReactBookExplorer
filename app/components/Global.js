be103cc0145a98f9d0018c72bee5341fe8df1852
Haris Sucipto <harisssucipto@gmail.com>
2017-05-07 19:53:03 +0700

    add search function

--- app/components/Global.js
+++ app/components/Global.js
@@ -3,8 +3,16 @@ import { FormGroup, FormControl, InputGroup, Glyphicon } from 'react-bootstrap';
 
 
 class Global extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      query: ''
+    }
+  }
+
+
   search() {
-    console.log('search for now')
+    console.log('search', this.state.query)
   }
 
   render() {
@@ -13,7 +21,16 @@ class Global extends Component {
         <h2>Book Explorer!</h2>
         <FormGroup>
           <InputGroup>
-            <FormControl type="text" placeholder="Search for a book" />
+            <FormControl
+              type="text"
+              placeholder="Search for a book"
+              onChange={event => this.setState({query: event.target.value})}
+              onKeyPress={event => {
+                if (event.key === 'Enter') {
+                  this.search();
+                }}
+              }
+            />
             <InputGroup.Addon onClick={() => this.search()}>
               <Glyphicon glyph="search"></Glyphicon>
             </InputGroup.Addon>
